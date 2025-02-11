51706ad1a0fea1ffcab425de3ca0c63ed8894d4a
Emmanuel Xs <emmanuelxs101@gmail.com>
2025-02-11 18:39:17 +0100

    chore: upgrade tailwind to v4

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Road_Rage, Roboto } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { jejuSerif, roadRage, roboto } from "@/lib/fonts";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,7 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${roadRage.variable} ${roboto.variable} ${jejuSerif.variable} antialiased bg-`}
       >
         {children}
       </body>
